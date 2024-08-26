a45c01d334697b63708ed6d5efb37a6f95e0f344
DESKTOP-RPG8R1V\User <manuelabg_0610@outlook.com>
2024-08-26 08:45:55 -0600

    First commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App"
 
-Vue.config.productionTip = false
+import store from './store/index'
+
+import AuthHandler from "./components/AuthHandler.vue";
+import ImageList from "./components/ImageList.vue";
+import UploadForm from "./components/UploadForm.vue";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/', component: ImageList
+        },
+        {
+           path: '/upload', component: UploadForm 
+        },
+        {
+            path: '/oauth2/callback', component: AuthHandler
+        }
+    ]
+})
 
 new Vue({
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
